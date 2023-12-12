3dd74d43b424be1f0939edc2626b7b48ae1af940
lSara-MM <sqmartinezm@gmail.com>
2023-12-12 13:02:32 +0100

    rework wip

--- ManganimeMobiles/manganime/lib/screens/CharactersScreen.dart
+++ ManganimeMobiles/manganime/lib/screens/CharactersScreen.dart
@@ -3,58 +3,19 @@ import 'package:manganime/screens/AnimeScreen.dart';
 import 'package:manganime/screens/MangaScreen.dart';
 import 'package:manganime/screens/UserScreen.dart';
 
+import 'package:manganime/widgets/nav_bar.dart';
+
 class CharacterScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Characters Screen'),
+        title: const Text('Characters Screen'),
       ),
-      body: SingleChildScrollView(
+      body: const SingleChildScrollView(
         child: Column(),
       ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.black87,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.person, color: Colors.white),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => UserScreen()),
-                );
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.my_library_books, color: Colors.white),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MangaScreen()),
-                );
-              },
-            ),
-            IconButton(
-              icon: Icon(Icons.tv_outlined, color: Colors.white),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => AnimeScreen()),
-                );
-              },
-            ),
-            IconButton(
-              icon: Icon(
-                Icons.list_alt,
-                color: Colors.orange,
-              ),
-              onPressed: () {},
-            ),
-          ],
-        ),
-      ),
+      bottomNavigationBar: NavBar(),
     );
   }
 }
