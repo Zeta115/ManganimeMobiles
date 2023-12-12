bf06b8d6955cc56ec41c059d44ec09f638097146
lSara-MM <sqmartinezm@gmail.com>
2023-12-12 13:59:36 +0100

    wip ahora probablemente peta, dejame acabar esto xd

--- ManganimeMobiles/manganime/lib/main.dart
+++ ManganimeMobiles/manganime/lib/main.dart
@@ -5,14 +5,22 @@ import 'package:manganime/screens/EpisodeInfoScreen.dart';
 import 'package:manganime/screens/MangaScreen.dart';
 import 'package:manganime/screens/UserScreen.dart';
 import 'package:manganime/widgets/nav_bar.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(AnimeMangaApp());
 }
 
-class AnimeMangaApp extends StatelessWidget {
+class AnimeMangaApp extends StatefulWidget {
   AnimeMangaApp({super.key});
+
+  @override
+  State<AnimeMangaApp> createState() => _AnimeMangaAppState();
+}
+
+class _AnimeMangaAppState extends State<AnimeMangaApp> {
   int currentScreen = 0;
+
   Set<Widget> screens_ = {
     CharacterScreen(),
     AnimeScreen(),
@@ -21,21 +29,19 @@ class AnimeMangaApp extends StatelessWidget {
     UserScreen()
   };
 
-  List<Map<String, Object>> screens = [
-    {"Character": CharacterScreen()},
-    {"Anime": AnimeScreen()},
-    {"EpisodeInfo": const EpisodeInfoScreen()},
-    {"Manga": CharacterScreen()},
-  ];
+  void setSelectedScreen(i) {
+    setState(() {
+      currentScreen = i;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          bottomNavigationBar: NavBar(
-            currentScreen: currentScreen,
-          ),
-          body: screens_.elementAt(currentScreen)),
+        bottomNavigationBar: NavBar(),
+        body: screens_.elementAt(currentScreen),
+      ),
     );
   }
 }
--- ManganimeMobiles/manganime/lib/widgets/nav_bar.dart
+++ ManganimeMobiles/manganime/lib/widgets/nav_bar.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class NavBar extends StatelessWidget {
   NavBar({
-    this.currentScreen = 0,
     super.key,
   });
 
-  int currentScreen;
-
   @override
   Widget build(BuildContext context) {
+    int currentScreen = context.read<int>();
     return BottomNavigationBar(
       backgroundColor: Colors.black87,
       currentIndex: currentScreen,
@@ -20,17 +19,18 @@ class NavBar extends StatelessWidget {
         navBarItem("Characters", Icons.dangerous),
         navBarItem("User", Icons.dangerous)
       ],
+      onTap: setSelectedScreen(),
     );
   }
+}
 
-  BottomNavigationBarItem navBarItem(String label, IconData icon) {
-    return BottomNavigationBarItem(
-      icon: Icon(icon),
-      activeIcon: Icon(
-        icon,
-        color: Colors.cyan,
-      ),
-      label: label,
-    );
-  }
+BottomNavigationBarItem navBarItem(String label, IconData icon) {
+  return BottomNavigationBarItem(
+    icon: Icon(icon),
+    activeIcon: Icon(
+      icon,
+      color: Colors.cyan,
+    ),
+    label: label,
+  );
 }
