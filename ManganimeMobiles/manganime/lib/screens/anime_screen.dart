c67b55b1ba59835a4aa8d89ee1fb5815bc1513f9
lSara-MM <sqmartinezm@gmail.com>
2024-01-10 13:09:16 +0100

    wip

--- ManganimeMobiles/manganime/lib/screens/anime_screen.dart
+++ ManganimeMobiles/manganime/lib/screens/anime_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:manganime/Widgets/search_bar.dart';
-import 'package:provider/provider.dart';
 
 import 'package:manganime/api.dart';
 import 'package:manganime/models/anime.dart';
@@ -11,9 +10,14 @@ import 'package:manganime/widgets/anime_header.dart';
 //https://pub.dev/packages/flutter_image_slideshow/example
 //import 'package:wp_pivot_flutter/wp_pivot_flutter.dart';
 
-class AnimeScreen extends StatelessWidget {
+class AnimeScreen extends StatefulWidget {
   const AnimeScreen({super.key});
 
+  @override
+  State<AnimeScreen> createState() => _AnimeScreenState();
+}
+
+class _AnimeScreenState extends State<AnimeScreen> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<List<Anime>>>(
@@ -32,59 +36,105 @@ class AnimeScreen extends StatelessWidget {
         }
         final List<Anime> listRecent = snapshot.data![0];
         final List<Anime> listTop = snapshot.data![1];
-        //final List<Anime> listPopular = snapshot.data![1];
+        final List<Anime> listUpcoming = snapshot.data![1];
         //final List<Anime> listByGenre = snapshot.data![1];
 
-        return Provider.value(
-          value: listRecent,
-          child: CustomScrollView(
-            slivers: [
-              SliverToBoxAdapter(
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Container(
-                            width: 110,
-                            height: 50,
-                            decoration: const BoxDecoration(
-                              image: DecorationImage(
-                                image:
-                                    AssetImage("assets/animeAppLogoFull.png"),
-                                fit: BoxFit.contain,
-                              ),
+        bool recent = true;
+        bool popular = false;
+        bool upcoming = false;
+
+        return CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          width: 110,
+                          height: 50,
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage("assets/animeAppLogoFull.png"),
+                              fit: BoxFit.contain,
                             ),
                           ),
-                          const SearchBarWidget(),
-                        ],
-                      ),
+                        ),
+                        const SearchBarWidget(),
+                      ],
                     ),
-                    CarouselTopAnimes(listTop: listTop),
-                    const Divider(),
-                    const Padding(
-                      padding: EdgeInsets.only(bottom: 10),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Section(name: "Latest episodes"),
-                          Section(name: "Most popular"),
-                          /*Text(
-                            "By genre",
-                            style: TextStyle(fontWeight: FontWeight.w500),
-                          ),*/
-                        ],
-                      ),
+                  ),
+                  CarouselTopAnimes(listTop: listTop),
+                  const Divider(),
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Section(name: "Latest episodes", active: true),
+                        Section(name: "Most popular", active: false),
+                        /*GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              recent = true;
+                              popular = false;
+                              upcoming = false;
+                            });
+                          },
+                          child: Column(
+                            children: [
+                              Section(name: "Latest episodes", active: recent),
+                              AnimeGrid(animeList: listRecent),
+                            ],
+                          ),
+                        ),
+
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              recent = false;
+                              popular = true;
+                              upcoming = false;
+                            });
+                          },
+                          child: Column(
+                            children: [
+                              Section(name: "Popular", active: popular),
+                              AnimeGrid(animeList: listTop),
+                            ],
+                          ),
+                        ),
+
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              recent = false;
+                              popular = false;
+                              upcoming = true;
+                            });
+                          },
+                          child: Column(
+                            children: [
+                              Section(name: "Upcoming", active: upcoming),
+                              AnimeGrid(animeList: listUpcoming),
+                            ],
+                          ),
+                        ),
+                      */
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              const AnimeGrid(),
-            ],
-          ),
+            ),
+            AnimeGrid(
+              animeList: listRecent,
+            )
+          ],
         );
       },
     );
@@ -92,9 +142,11 @@ class AnimeScreen extends StatelessWidget {
 }
 
 class Section extends StatelessWidget {
-  const Section({super.key, required this.name});
+  const Section({super.key, required this.name, required this.active});
 
   final String name;
+  final bool active;
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -105,7 +157,10 @@ class Section extends StatelessWidget {
             name,
             style: const TextStyle(fontWeight: FontWeight.w500),
           ),
-          Divider(thickness: 5, )
+          Divider(
+            thickness: 5,
+            color: active ? Colors.cyan : Colors.grey,
+          )
         ],
       ),
     );
